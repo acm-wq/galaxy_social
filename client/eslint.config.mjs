0632a56ef7b24e2e5f05dbbe3be41e9fa8d29be6
acm-wq <camisglhdadae@gmail.com>
2025-04-08 20:38:50 +0700

    #9 📡 Add eslint-config-eslint

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigESLint from "eslint-config-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,4 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default eslintConfig;
+export default [
+  ...eslintConfig,
+  ...eslintConfigESLint
+];
