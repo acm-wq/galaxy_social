4f5df7c3babf0c25140ea84ec14c2721e2b23ed4
acm-wq <camisglhdadae@gmail.com>
2025-04-26 01:03:03 +0700

    #17 📡 Add doc for stars_controller

--- server/app/controllers/stars_controller.rb
+++ server/app/controllers/stars_controller.rb
@@ -1,3 +1,37 @@
+# The StarsController handles actions related to managing stars in the application.
+# It provides endpoints for retrieving random stars, fetching specific stars by ID,
+# and creating new stars.
+
+# Actions:
+# - random: Fetches a random star that is not in the excluded list provided via parameters.
+#           Responds with an error if no more stars are available.
+#           Route: GET /stars/random
+#           Params:
+#             - list_stars (optional): Array of star names to exclude from the random selection.
+#           Responses:
+#             - 200 OK: Returns the random star data in JSON format.
+#             - 404 Not Found: If no more stars are available.
+
+# - show: Fetches a specific star by its ID from the Redis cache.
+#         Responds with an error if the star is not found.
+#         Route: GET /stars/:id
+#         Params:
+#           - id (required): The ID of the star to fetch.
+#         Responses:
+#           - 200 OK: Returns the star data in JSON format.
+#           - 404 Not Found: If the star is not found.
+
+# - create: Creates a new star with the provided parameters.
+#           Responds with the created star's ID and a success message if successful.
+#           Route: POST /stars
+#           Params:
+#             - star (required): A hash containing the following attributes:
+#               - name (string): The name of the star.
+#               - password (string): The password associated with the star.
+#               - type_star (string): The type/category of the star.
+#           Responses:
+#             - 201 Created: If the star is successfully created.
+#             - 422 Unprocessable Entity: If the star creation fails.
 class StarsController < ApplicationController
   # GET /stars/random
   def random
